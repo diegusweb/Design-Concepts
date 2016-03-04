d4b259b47ca97d168385de722640951f5f4e11a5
Diego Rueda <diego.rueda2@gmail.com>
2016-03-04 15:57:11 -0400

    add adapter and integrated in activity main

--- app/src/main/java/app/diegusweb/designconcepts/InicioFragment.java
+++ app/src/main/java/app/diegusweb/designconcepts/InicioFragment.java
@@ -2,16 +2,24 @@ package app.diegusweb.designconcepts;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import app.diegusweb.designconcepts.Adapter.AdaptadorInicio;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InicioFragment extends Fragment {
+    private RecyclerView reciclador;
+    private LinearLayoutManager layoutManager;
+    private AdaptadorInicio adaptador;
 
 
     public InicioFragment() {
@@ -22,8 +30,15 @@ public class InicioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        reciclador = (RecyclerView) view.findViewById(R.id.reciclador);
+        layoutManager = new LinearLayoutManager(getActivity());
+        reciclador.setLayoutManager(layoutManager);
+
+        adaptador = new AdaptadorInicio();
+        reciclador.setAdapter(adaptador);
+        return view;
     }
 
 
