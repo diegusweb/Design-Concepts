d0f3fee9d34d3a2fe1ae6886364acc478c33bac3
Diego Rueda <diego.rueda2@gmail.com>
2016-03-04 16:47:27 -0400

    added more data for tabs

--- app/src/main/java/app/diegusweb/designconcepts/CuentaFragment.java
+++ app/src/main/java/app/diegusweb/designconcepts/CuentaFragment.java
@@ -3,12 +3,19 @@ package app.diegusweb.designconcepts;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Fragment;
+
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +25,7 @@ public class CuentaFragment extends Fragment {
 
     private AppBarLayout appBar;
     private TabLayout pestanas;
+    private ViewPager viewPager;
 
     public CuentaFragment() {
     }
@@ -29,6 +37,11 @@ public class CuentaFragment extends Fragment {
 
         if (savedInstanceState == null) {
             insertarTabs(container);
+
+            // Setear adaptador al viewpager.
+            viewPager = (ViewPager) view.findViewById(R.id.pager);
+            poblarViewPager(viewPager);
+            pestanas.setupWithViewPager(viewPager);
         }
 
         return view;
@@ -42,9 +55,50 @@ public class CuentaFragment extends Fragment {
         appBar.addView(pestanas);
     }
 
+    private void poblarViewPager(ViewPager viewPager) {
+        AdaptadorSecciones adapter = new AdaptadorSecciones(getFragmentManager());
+        adapter.addFragment(new FragmentoPerfil(), getString(R.string.titulo_tab_perfil));
+        adapter.addFragment(new FragmentoDirecciones(), getString(R.string.titulo_tab_direcciones));
+        adapter.addFragment(new FragmentoTarjetas(), getString(R.string.titulo_tab_tarjetas));
+        viewPager.setAdapter(adapter);
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         appBar.removeView(pestanas);
+    }
+
+    /**
+     * Un {@link FragmentStatePagerAdapter} que gestiona las secciones, fragmentos y
+     * títulos de las pestañas
+     */
+    public class AdaptadorSecciones extends FragmentStatePagerAdapter {
+        private final List<Fragment> fragmentos = new ArrayList<>();
+        private final List<String> titulosFragmentos = new ArrayList<>();
+
+        public AdaptadorSecciones(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return fragmentos.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentos.size();
+        }
+
+        public void addFragment(android.support.v4.app.Fragment fragment, String title) {
+            fragmentos.add(fragment);
+            titulosFragmentos.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titulosFragmentos.get(position);
+        }
     }
 }
--- app/src/main/java/app/diegusweb/designconcepts/Model/Comida.java
+++ app/src/main/java/app/diegusweb/designconcepts/Model/Comida.java
@@ -34,6 +34,24 @@ public class Comida {
         COMIDAS_POPULARES.add(new Comida(12f, "Sushi Extremo", R.drawable.sushi));
         COMIDAS_POPULARES.add(new Comida(9, "Sandwich Deli", R.drawable.sandwich));
         COMIDAS_POPULARES.add(new Comida(34f, "Lomo De Cerdo Austral", R.drawable.lomo_cerdo));
+
+        PLATILLOS.add(new Comida(5, "Camarones Tismados", R.drawable.camarones));
+        PLATILLOS.add(new Comida(3.2f, "Rosca Herbárea", R.drawable.rosca));
+        PLATILLOS.add(new Comida(12f, "Sushi Extremo", R.drawable.sushi));
+        PLATILLOS.add(new Comida(9, "Sandwich Deli", R.drawable.sandwich));
+        PLATILLOS.add(new Comida(34f, "Lomo De Cerdo Austral", R.drawable.lomo_cerdo));
+
+        BEBIDAS.add(new Comida(3, "Taza de Café", R.drawable.cafe));
+        BEBIDAS.add(new Comida(12, "Coctel Tronchatoro", R.drawable.coctel));
+        BEBIDAS.add(new Comida(5, "Jugo Natural", R.drawable.jugo_natural));
+        BEBIDAS.add(new Comida(24, "Coctel Jordano", R.drawable.coctel_jordano));
+        BEBIDAS.add(new Comida(30, "Botella Vino Tinto Darius", R.drawable.vino_tinto));
+
+        POSTRES.add(new Comida(2, "Postre De Vainilla", R.drawable.postre_vainilla));
+        POSTRES.add(new Comida(3, "Flan Celestial", R.drawable.flan_celestial));
+        POSTRES.add(new Comida(2.5f, "Cupcake Festival", R.drawable.cupcakes_festival));
+        POSTRES.add(new Comida(4, "Pastel De Fresa", R.drawable.pastel_fresa));
+        POSTRES.add(new Comida(5, "Muffin Amoroso", R.drawable.muffin_amoroso));
     }
 
     public float getPrecio() {
